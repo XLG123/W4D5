723f732843e37eea3cdc8236eadc39f546da1284
XLG123 <xiaolin.guan123@gmail.com>
2023-05-26 15:20:45 -0400

    my_transpose completed

--- practice-for-ch-ruby-first-tdd-long-practice-main/lib/my_array.rb
+++ practice-for-ch-ruby-first-tdd-long-practice-main/lib/my_array.rb
@@ -17,5 +17,21 @@ class Array
     return res.sort
   end 
 
+  def my_transpose
+		transposed_arr = []
+		i = 0
+		j = 0
+		while j < self[0].length
+			new_sub_arr = []
+			while i < self.length
+				new_sub_arr << self[i][j]
+				i += 1
+			end
+			transposed_arr << new_sub_arr
+			i = 0
+			j += 1
+		end
+		transposed_arr
+  end
 
 end 
--- practice-for-ch-ruby-first-tdd-long-practice-main/spec/my_array_spec.rb
+++ practice-for-ch-ruby-first-tdd-long-practice-main/spec/my_array_spec.rb
@@ -30,5 +30,25 @@ describe "Array" do
       expect(array2.two_sum).to eq([[0,4],[2,3]])
     end
   end
+
+  describe "my_transpose" do
+    let(:array3) {[ [0, 1, 2],
+                    [3, 4, 5],
+                    [6, 7, 8] ]}
+    it "accepts an array as an argument" do 
+      expect{Array.new}.to_not raise_error
+    end
+
+    it "returns an empty array when there are no elements" do 
+      expect(Array.new).to be_empty
+    end
+
+    it "returns an array where the rows and columns are interchanged" do
+      expect(array3.my_transpose).to eq([[0, 3, 6],
+                                         [1, 4, 7],
+                                         [2, 5, 8] ])
+    end
+
+  end
 end
 
