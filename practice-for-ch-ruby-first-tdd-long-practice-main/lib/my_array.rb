390b39648e225a4e78afd48a1e53eda31b9fb933
Jet <jetpoke1011@gmail.com>
2023-05-26 12:14:59 -0400

    two sum specs

--- practice-for-ch-ruby-first-tdd-long-practice-main/lib/my_array.rb
+++ practice-for-ch-ruby-first-tdd-long-practice-main/lib/my_array.rb
@@ -4,4 +4,18 @@ class Array
     self.each { |ele| hash[ele] = 1 }
     hash.keys
   end
-end
+
+  def two_sum
+    hash = {}
+    res = []
+    self.each_with_index do |ele,idx|
+      if hash[ele]
+        res << [hash[ele],idx]
+      end 
+      hash[ele * -1] = idx 
+    end 
+    return res.sort
+  end 
+
+
+end 
--- practice-for-ch-ruby-first-tdd-long-practice-main/spec/my_array_spec.rb
+++ practice-for-ch-ruby-first-tdd-long-practice-main/spec/my_array_spec.rb
@@ -3,6 +3,7 @@ require "my_array"
 describe "Array" do
   describe "my_uniq" do
     let(:array) { [1, 2, 2, 3, 3, 333, 333, 4, 5, 5]}
+    let(:array2) {[-1,0,2,-2,1]}
     it "accepts an array as an argument" do
       expect { Array.new }.to_not raise_error
     end
@@ -15,4 +16,16 @@ describe "Array" do
       expect(array.my_uniq).to eq([1, 2, 3, 333, 4, 5])
     end
   end
+
+describe "two_sum" do 
+  it "accepts an array as an argument" do 
+    expect {Array.new}.to_not raise_error
+  end 
+  it "returns an empty array when there are no elements" do 
+    expect(Array.new).to be_empty
+  end 
+  it "returns a array with all pairs with positions where the sum is 0"
+    expect {array2.two_sum}.to eq([[0,4],[2,3]])
 end
+end
+
